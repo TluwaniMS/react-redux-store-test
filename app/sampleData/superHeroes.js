80a38a2f71d0256280ab9eda3f01ef3236dde5bd
tluwaniMS <tluwani2020@gmail.com>
2022-03-13 21:36:12 +0200

    adding-sample-data

---
+++ app/sampleData/superHeroes.js
@@ -0,0 +1,13 @@
+export const SuperHeroes = [
+  {
+    _id: "61f58a1d2f87a0e538236b28",
+    name: "Elephant Man",
+    superPowers: []
+  },
+  { _id: "61f58a1d2f87a0e538236b29", name: "Imhotep", superPowers: [] },
+  {
+    _id: "61f58a1d2f87a0e538236b2a",
+    name: "Shaka Khan",
+    superPowers: []
+  }
+];
---
+++ app/sampleData/superPowers.js
@@ -0,0 +1,28 @@
+export const SuperPowers = [
+  {
+    _id: "61f58a1d2f87a0e538236b2b",
+    power: "The Pyramid Punch",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    _id: "61f58a1d2f87a0e538236b2c",
+    power: "The Iklwa Injectors",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    _id: "61f58a1d2f87a0e538236b2d",
+    power: "Stomp Spin",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    _id: "61f58a1d2f87a0e538236b2e",
+    power: "Halt Horn",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    _id: "61f58abe03175729b2ffb237",
+    power: "The Ghengis Grip",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  }
+];
+
