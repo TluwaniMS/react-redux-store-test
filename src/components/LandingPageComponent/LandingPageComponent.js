9d0294ee93c810562dc59c5eb81aed52f8b3135c
tluwaniMS <tluwani2020@gmail.com>
2022-03-15 05:38:22 +0200

    addding-displays-in-main-landing-page-component

--- src/components/LandingPageComponent/LandingPageComponent.js
+++ src/components/LandingPageComponent/LandingPageComponent.js
@@ -1,7 +1,16 @@
 import React from "react";
+import MoviesDisplay from "../../displays/MoviesDisplay/MoviesDisplay";
+import SuperHeroesDisplay from "../../displays/SuperHeroesDisplay/SuperHeroesDisplay";
+import SuperPowersDisplay from "../../displays/SuperPowersDisplay/SuperPowersDisplay";
 
 const LandingPageComponent = () => {
-  return <div></div>;
+  return (
+    <div>
+      <MoviesDisplay />
+      <SuperHeroesDisplay />
+      <SuperPowersDisplay />
+    </div>
+  );
 };
 
 export default LandingPageComponent;
