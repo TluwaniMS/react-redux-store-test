b693b6d440abd0ea97a66fee700f26802f8a65ca
tluwaniMS <tluwani2020@gmail.com>
2022-03-15 18:24:41 +0200

    adding-view-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import DetailedMovieComponent from "./components/DetailedMovieComponent/DetailedMovieComponent";
+import DetailedSuperHeroComponent from "./components/DetailedSuperHeroComponent/DetailedSuperHeroComponent";
+import LandingPageComponent from "./components/LandingPageComponent/LandingPageComponent";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPageComponent />} />
+        <Route path="/detailed-movie-view" element={<DetailedMovieComponent />} />
+        <Route path="/detailed-super-view" element={<DetailedSuperHeroComponent />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
