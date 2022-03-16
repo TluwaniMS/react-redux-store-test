cea8c6c93bf502ff0d009c1eca658541523163aa
tluwaniMS <tluwani2020@gmail.com>
2022-03-16 23:38:21 +0200

    first-phase-of-adding-dispatch-test

--- src/displays/SuperPowersDisplay/SuperPowersDisplay.js
+++ src/displays/SuperPowersDisplay/SuperPowersDisplay.js
@@ -3,8 +3,11 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
+import { useDispatch } from "react-redux";
 
 const SuperPowersDisplay = ({ superPower }) => {
+  const dispatch = useDispatch()
+
   return (
     <Card variant="outlined">
       <CardContent>
