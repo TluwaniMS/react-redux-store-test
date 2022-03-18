551728f5e8e9373fd5b8013af7ca8e8369c14336
tluwaniMS <tluwani2020@gmail.com>
2022-03-18 19:16:39 +0200

    adding-form-creation-displays

---
+++ src/displays/MovieCreationFormDisplay/MovieCreationFormDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieCreationFormDisplay = () => {
+  return <div></div>;
+};
+
+export default MovieCreationFormDisplay;
---
+++ src/displays/SuperPowerCreationFormDisplay/SuperPowerCreationFormDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SuperPowerCreationFormDisplay = () => {
+  return <div></div>;
+};
+
+export default SuperPowerCreationFormDisplay;
