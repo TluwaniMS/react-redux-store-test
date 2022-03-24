9cc25713165bd6146c733f3e490992164b1be5fb
tluwaniMS <tluwani2020@gmail.com>
2022-03-24 19:55:51 +0200

    adding-lead-character-field-to-movies

--- src/app/sampleData/movies.js
+++ src/app/sampleData/movies.js
@@ -2,16 +2,19 @@ export const Movies = [
   {
     _id: "61f58a1d2f87a0e538236b25",
     title: "The Adventures of Elephant Man",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    leadCharacters: []
   },
   {
     _id: "61f58a1d2f87a0e538236b26",
     title: "The Resurrection of The Cryptonites",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    leadCharacters: []
   },
   {
     _id: "61f58a1d2f87a0e538236b27",
     title: "The Final Fight",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    leadCharacters: []
   }
 ];
