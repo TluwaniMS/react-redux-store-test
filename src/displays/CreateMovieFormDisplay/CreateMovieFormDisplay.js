62e4d57e8b7526fd435d67fbcfb31456e51d9f0c
tluwaniMS <tluwani2020@gmail.com>
2022-03-25 14:54:27 +0200

    adding-form-display

---
+++ src/displays/CreateMovieFormDisplay/CreateMovieFormDisplay.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const CreateMovieFormDisplay = () => {
+  return <div></div>;
+};
+
+export default CreateMovieFormDisplay;
---
+++ src/displays/CreateSuperHeroFormDisplay/CreateSuperHeroFormDisplay.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const CreateSuperHeroFormDisplay = () => {
+  return <div></div>;
+};
+
+export default CreateSuperHeroFormDisplay;
